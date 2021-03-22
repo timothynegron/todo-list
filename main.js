d4935ba491e36eb77642fca16d3cc003d4c20454
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-21 20:37:34 -0400

    Adds line through list text feature.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// ┌──────────────────────┐
+// │   Global Variables   │	
+// └──────────────────────┘
+// TODO: Put in object.
+// let toDoList = {};
+// let listNumber = 0;
+
 // ┌──────────────────┐
 // │   Main Program   │	
 // └──────────────────┘
@@ -18,5 +25,11 @@ function setEventListeners() {
 function addTextToList() {
     let newLi = document.createElement("li");
     newLi.textContent = document.querySelector("input").value;
+    newLi.addEventListener("click", liToLineThrough);
     document.querySelector("ul").appendChild(newLi);
+    document.querySelector("input").value = "";
+}
+
+function liToLineThrough() {
+    event.target.style.textDecoration = "line-through";
 }
